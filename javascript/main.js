26072883770fb2ad3dafc9feec9472c75d094191
pepe <egivercito99@gmail.com>
2021-03-26 02:24:36 -0300

    correccion-2

--- javascript/main.js
+++ javascript/main.js
@@ -4,11 +4,11 @@ let number = [Math.floor(Math.random() *100)]
 btn.addEventListener("click", function(){
     let input = document.getElementById('userInput').value;
     if (input == number ){
-        output.innerHTML = `Lo adivinaste bien, tu número era ${number}`
+        output.innerHTML = ` adivinaste bien, tu número era ${number}`
     } else if (input < number){
-        output.innerHTML = "El numero que adivinaste es demaciado bajo"
+        output.innerHTML = "El número que adivinaste es demasiado bajo"
     };
     if (input > number){
-        output.innerHTML =  "El numero que adivinaste es demaciado alto"
+        output.innerHTML =  "El número que adivinaste es demasiado alto"
     }
 })
